8e3174b77d4c282daa25d1cb3d2d65638c2b077d
bugrabolat <65684263+bugrabolat@users.noreply.github.com>
2022-05-09 17:59:41 +0300

    Add files via upload

---
+++ hesap.js
@@ -0,0 +1,67 @@
+var screen = document.getElementById("screen");
+var first = " ";
+var second = " ";
+var selectedOpr = " ";
+
+
+function onClickButton(element, x) {
+  if(screen.value == 0){
+    screen.value ="";
+  }
+  switch (x) {
+    case '0':
+      screen.value += element.value;
+      break;
+    case '1':
+      screen.value += element.value;
+      break;
+    case '2':
+      screen.value += element.value;
+      break;
+    case '3':
+      screen.value += element.value;
+      break;
+    case '4':
+      screen.value += element.value;
+      break;
+    case '5':
+      screen.value += element.value;
+      break;
+    case '6':
+      screen.value += element.value;
+      break;
+    case '7':
+      screen.value += element.value;
+      break;
+    case '8':
+      screen.value += element.value;
+      break;
+    case '9':
+      screen.value += element.value;
+      break;
+  }
+}
+function onClickClearButton() {
+  screen.value = 0;
+}
+function onClickPositiveButton() {
+  if (screen.value) {
+    screen.value
+  }
+}
+function onClickSepelator(element,y) {
+ switch (y) {
+   case "+":
+    selectedOpr += element.value
+     break;
+     case "-":
+      selectedOpr += element.value
+     break;
+     case "*":
+      selectedOpr += element.value
+     break;
+     case "/":
+      selectedOpr += element.value
+     break;
+ }
+}
