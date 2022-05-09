3030e4d1e3bf5a566313bff2f9be7e3a74087c97
bugrabolat <65684263+bugrabolat@users.noreply.github.com>
2022-05-09 03:42:20 +0300

    hesap

---
+++ hesap makinesi/hesap.js
@@ -0,0 +1,23 @@
+function onClickButton(element,x){
+    var screen = document.getElementById("screen");
+    switch (x) {
+        case 'a':
+            screen.value += element.value;
+            break;
+    case 'b':
+        screen.value += element.value;
+      break;
+      case 'c':
+        screen.value += element.value;
+      break;
+      case 'd':
+        screen.value += element.value;
+      break;
+      case 'e':
+        screen.value += element.value;
+      break;
+      case 'f':
+        screen.value += element.value;
+      break;
+    }
+}
