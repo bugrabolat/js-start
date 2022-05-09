6d9e219e79d31ad07fcdeb86765aef67a2949db5
bugrabolat <65684263+bugrabolat@users.noreply.github.com>
2022-05-09 18:51:57 +0300

    Add files via upload

--- hesap makinesi/hesap.js
+++ hesap makinesi/hesap.js
@@ -1,23 +1,81 @@
-function onClickButton(element,x){
-    var screen = document.getElementById("screen");
-    switch (x) {
-        case 'a':
-            screen.value += element.value;
-            break;
-    case 'b':
-        screen.value += element.value;
+var screen = document.getElementById("screen");
+var first ='';
+var second ='';
+var selectedOpr ="";
+var sonuc = '';
+
+
+function onClickButton(element, x) {
+  if(screen.value == 0){
+    screen.value ="";
+  }
+  switch (x) {
+    case '0':
+      screen.value += element.value;
       break;
-      case 'c':
-        screen.value += element.value;
+    case '1':
+      screen.value += element.value;
       break;
-      case 'd':
-        screen.value += element.value;
+    case '2':
+      screen.value += element.value;
       break;
-      case 'e':
-        screen.value += element.value;
+    case '3':
+      screen.value += element.value;
       break;
-      case 'f':
-        screen.value += element.value;
+    case '4':
+      screen.value += element.value;
       break;
-    }
+    case '5':
+      screen.value += element.value;
+      break;
+    case '6':
+      screen.value += element.value;
+      break;
+    case '7':
+      screen.value += element.value;
+      break;
+    case '8':
+      screen.value += element.value;
+      break;
+    case '9':
+      screen.value += element.value;
+      break;
+  }
+}
+function onClickClearButton() {
+  screen.value = 0;
+}
+function onClickPositiveButton() {
+  if (screen.value) {
+    screen.value
+  }
+}
+function onClickSepelator(element,y) {
+ switch (y) {
+   case "+":
+    selectedOpr += element.value;
+     break;
+     case "-":
+      selectedOpr += element.value;
+     break;
+     case "*":
+      selectedOpr += element.value;
+     break;
+     case "/":
+      selectedOpr += element.value;
+     break;
+ }
+ if(selectedOpr ==""){
+   first += screen.value;
+ }else if(selectedOpr !==''){
+   second += screen.value;
+ }
+ console.log(first);
+}
+function hesapla(first,selectedOpr,second){
+  if(selectedOpr=="+"){
+    sonuc = first + second;
+  }
+  console.log(sonuc); 
+  
 }
